cdcb9d23a5bfa6a59f8ab8a6599d209c2af493eb
krishnansh-manprax <krishnansh.kapoor@manprax.com>
2025-07-16 15:44:33 +0530

    [UPD]Version

--- website_ora_elearning/__manifest__.py
+++ website_ora_elearning/__manifest__.py
@@ -5,7 +5,7 @@
     'category': 'Website/eLearning',
     'summary': 'Manage and publish an eLearning platform',
     'sequence': 10,
-    'version': '2.5',
+    'version': '2.6',
     'website': 'https://www.manprax.com',
     'author': 'ManpraX Software LLP',
     'depends': ['website_slides'],
